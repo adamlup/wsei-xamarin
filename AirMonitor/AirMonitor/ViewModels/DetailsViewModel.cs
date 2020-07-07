329d83371ec2e346119772946df15bb790c9f2cf
adamlup <31212931+adamlup@users.noreply.github.com>
2020-07-07 22:24:53 +0200

    lab3 implemetation

--- AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
+++ AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
@@ -1,122 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Runtime.CompilerServices;
-using System.Text;
-using Xamarin.Forms;
+using System.Linq;
+using AirMonitor.Models;
 
 namespace AirMonitor.ViewModels
 {
-    class DetailsViewModel : INotifyPropertyChanged
+    class DetailsViewModel : BaseViewModel
     {
      
-        private int _CAQI;
+        private int _CAQI = 57;
+        private string _caqiTitle = "Świetna jakość!";
+        private string _caqiDescription = "Możesz bezpiecznie wyjść z domu bez swojej maski anty-smogowej i nie bać się o swoje zdrowie.";
         private int _pm25 = 34;
         private int _pm25Percent = 137;
         private int _pm10 = 67;
         private int _pm10Percent = 135;
-        private double _wilgotnosc = 0.95;
-        private int _cisnienie = 1027;
-        public event PropertyChangedEventHandler PropertyChanged;
+        private double _humidityPercent = 29;
+        private int _pressure = 1027;
+
+        public DetailsViewModel() { }
+
+        private Measurement _item;
+        public Measurement Item
+        {
+            get => _item;
+            set
+            {
+                SetProperty(ref _item, value);
+
+                UpdateProperties();
+            }
+        }
+
+        private void UpdateProperties()
+        {
+            if (Item?.Current == null) return;
+            var current = Item?.Current;
+            var index = current.Indexes?.FirstOrDefault(c => c.Name == "AIRLY_CAQI");
+            var values = current.Values;
+            var standards = current.Standards;
+
+            CAQI = (int)Math.Round(index?.Value ?? 0);
+            CaqiTitle = index.Description;
+            CaqiDescription = index.Advice;
+            Pm25 = (int)Math.Round(values?.FirstOrDefault(s => s.Name == "PM25")?.Value ?? 0);
+            Pm10 = (int)Math.Round(values?.FirstOrDefault(s => s.Name == "PM10")?.Value ?? 0);
+            HumidityPercent = (int)Math.Round(values?.FirstOrDefault(s => s.Name == "HUMIDITY")?.Value ?? 0);
+            Pressure = (int)Math.Round(values?.FirstOrDefault(s => s.Name == "PRESSURE")?.Value ?? 0);
+            Pm25Percent = (int)Math.Round(standards?.FirstOrDefault(s => s.Pollutant == "PM25")?.Percent ?? 0);
+            Pm10Percent = (int)Math.Round(standards?.FirstOrDefault(s => s.Pollutant == "PM10")?.Percent ?? 0);
+        }
+
 
         public int CAQI
         {
-            get
-            {
-                return _CAQI;
-            }
-            set
-            {
-                _CAQI = value;
-                OnPropertyChanged();
-            }
+            get => _CAQI;
+            set => SetProperty(ref _CAQI, value);
+        }
+
+        public string CaqiTitle
+        {
+            get => _caqiTitle;
+            set => SetProperty(ref _caqiTitle, value);
+        }
+
+        public string CaqiDescription
+        {
+            get => _caqiDescription;
+            set => SetProperty(ref _caqiDescription, value);
         }
 
         public int Pm25
         {
-            get
-            {
-                return _pm25;
-            }
-            set
-            {
-                _pm25 = value;
-                OnPropertyChanged();
-            }
+            get => _pm25;
+            set => SetProperty(ref _pm25, value);
         }
 
         
         public int Pm25Percent
         {
-            get
-            {
-                return _pm25Percent;
-            }
-            set
-            {
-                _pm25Percent = value;
-                OnPropertyChanged();
-            }
+            get => _pm25Percent;
+            set => SetProperty(ref _pm25Percent, value);
         }
 
 
         public int Pm10
         {
-            get
-            {
-                return _pm10;
-            }
-            set
-            {
-                _pm10 = value;
-                OnPropertyChanged();
-            }
+            get => _pm10;
+            set => SetProperty(ref _pm10, value);
         }
 
         public int Pm10Percent
         {
-            get
-            {
-                return _pm10Percent;
-            }
-            set
-            {
-                _pm10Percent = value;
-                OnPropertyChanged();
-            }
+            get => _pm10Percent;
+            set => SetProperty(ref _pm10Percent, value);
         }
-        public double Wilgotnosc
+        public double HumidityPercent
         {
-            get
-            {
-                return _wilgotnosc;
-            }
-            set
-            {
-                _wilgotnosc = value;
-                OnPropertyChanged();
-            }
+            get => _humidityPercent;
+            set => SetProperty(ref _humidityPercent, value);
         }
 
-        public int Cisnienie
+        public int Pressure
         {
-            get
-            {
-                return _cisnienie;
-            }
-            set
-            {
-                _cisnienie = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public DetailsViewModel() { }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            get => _pressure;
+            set => SetProperty(ref _pressure, value);
         }
     }
 }
--- AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
+++ AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using AirMonitor.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirMonitor.Models;
+using AirMonitor.ViewModels;
 using Xamarin.Forms;
 
 namespace AirMonitor.Views
@@ -14,10 +15,12 @@ namespace AirMonitor.Views
     [DesignTimeVisible(false)]
     public partial class DetailsPage : ContentPage
     {
-        public DetailsPage()
+        public DetailsPage(Measurement item)
         {
             InitializeComponent();
 
+            var vm = BindingContext as DetailsViewModel;
+            vm.Item = item;
         }
 
         private void Help_Clicked(object sender, EventArgs e)
